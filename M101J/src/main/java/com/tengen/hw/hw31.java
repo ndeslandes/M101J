7c60596aa30509832625b9493208e22ac86165a8
ndeslandes <ndeslandes@iweb.com>
2013-03-14 10:02:16 -0400

    end of week 3

--- M101J/src/main/java/com/tengen/hw/hw31.java
+++ M101J/src/main/java/com/tengen/hw/hw31.java
@@ -6,38 +6,39 @@ import java.net.UnknownHostException;
 import java.util.List;
 
 public class hw31 {
-    public static void main(String[] args) throws UnknownHostException {
-        MongoClient client = new MongoClient();
-        DB db = client.getDB("school");
-        DBCollection students = db.getCollection("students");
+   public static void main(String[] args) throws UnknownHostException {
+      MongoClient client = new MongoClient();
+      DB db = client.getDB("school");
+      DBCollection students = db.getCollection("students");
 
-        System.out.println(students.count());
+      System.out.println(students.count());
 
-        DBCursor cursor = students.find();
-        try {
-            while (cursor.hasNext()) {
-                DBObject student = cursor.next();
-                List<DBObject> scores = (List<DBObject>) student.get("scores");
-                DBObject lowestHomeworkScoreDoc = getLowestHoweworkScoreDocument(scores);
-                scores.remove(lowestHomeworkScoreDoc);
-                students.update(student, new BasicDBObject("$set", new BasicDBObject("scores", scores)));
+      DBCursor cursor = students.find();
+      try {
+         while (cursor.hasNext()) {
+            DBObject student = cursor.next();
+            Integer studentId = (Integer) student.get("_id");
+            List<DBObject> scores = (List<DBObject>) student.get("scores");
+            DBObject lowestHomeworkScoreDoc = getLowestHoweworkScoreDocument(scores);
+            scores.remove(lowestHomeworkScoreDoc);
+            students.update(new BasicDBObject("_id", studentId), new BasicDBObject("$set", new BasicDBObject("scores", scores)));
+         }
+      } finally {
+         cursor.close();
+      }
+   }
+
+   private static DBObject getLowestHoweworkScoreDocument(List<DBObject> scores) {
+      DBObject lowestHomeworkScoreDoc = null;
+      for (DBObject scoreDoc : scores) {
+         String type = (String) scoreDoc.get("type");
+         if (type.equals("homework")) {
+            Double score = (Double) scoreDoc.get("score");
+            if (lowestHomeworkScoreDoc == null || score < (Double) lowestHomeworkScoreDoc.get("score")) {
+               lowestHomeworkScoreDoc = scoreDoc;
             }
-        } finally {
-            cursor.close();
-        }
-    }
-
-    private static DBObject getLowestHoweworkScoreDocument(List<DBObject> scores) {
-        DBObject lowestHomeworkScoreDoc = null;
-        for (DBObject scoreDoc : scores) {
-            String type = (String) scoreDoc.get("type");
-            if (type.equals("homework")) {
-                Double score = (Double) scoreDoc.get("score");
-                if (lowestHomeworkScoreDoc == null || score < (Double) lowestHomeworkScoreDoc.get("score")) {
-                    lowestHomeworkScoreDoc = scoreDoc;
-                }
-            }
-        }
-        return lowestHomeworkScoreDoc;
-    }
+         }
+      }
+      return lowestHomeworkScoreDoc;
+   }
 }
